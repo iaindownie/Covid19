0756824de70e250d8ddb1781081b83cb207daf01
iain downie <iain.downie@gmail.com>
2021-05-07 12:38:03 +0100

    Even better graphing!

--- app/src/main/java/bto/android/covid_19/CountryStats.java
+++ app/src/main/java/bto/android/covid_19/CountryStats.java
@@ -1,9 +1,12 @@
 package bto.android.covid_19;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -20,9 +23,19 @@ import org.json.JSONObject;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import lecho.lib.hellocharts.gesture.ZoomType;
+import lecho.lib.hellocharts.listener.LineChartOnValueSelectListener;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.LineChartView;
 
 public class CountryStats extends AppCompatActivity {
 
@@ -30,16 +43,20 @@ public class CountryStats extends AppCompatActivity {
     private static final String TAG = "CountryStats";
     private Bundle bundle;
     private String countrySlug, countryTextTitle = "";
-
-    private GraphView graph;
-    private LineGraphSeries<DataPoint> series;
     private TextView countryTitle;
+    private Activity activity;
+
+    //private GraphView graph;
+    //private LineGraphSeries<DataPoint> series;
+
+    private LineChartView chart;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_country_stats);
+        activity = this;
 
         Intent intent = this.getIntent();
         if (intent != null) {
@@ -50,7 +67,9 @@ public class CountryStats extends AppCompatActivity {
 
         countryTitle = findViewById(R.id.country_title);
         countryTitle.setText("Daily new cases: " + countryTextTitle);
-        graph = findViewById(R.id.graph);
+        //graph = findViewById(R.id.graph);
+        chart = findViewById(R.id.chart);
+        chart.setOnValueTouchListener(new ValueTouchListener());
 
         volleyJsonArrayRequest(JOHN_HOPKINS_API_BASE_URL + countrySlug + "/status/confirmed");
     }
@@ -70,7 +89,9 @@ public class CountryStats extends AppCompatActivity {
                     public void onResponse(JSONArray response) {
                         //Log.d("INFO", String.valueOf(response.length()));
 
-                        DataPoint[] dataPoints = new DataPoint[response.length()];
+                        //DataPoint[] dataPoints = new DataPoint[response.length()];
+
+                        List<PointValue> values = new ArrayList<PointValue>();
 
                         //DateFormat fmt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 
@@ -93,26 +114,49 @@ public class CountryStats extends AppCompatActivity {
                                 //}
                                 if (daily > maxY) maxY = daily;
                                 //Log.d("INFO", sdf.format(fmt.parse(date)) + " " + runningTotal + " " + daily);
-                                dataPoints[i] = new DataPoint(i, daily);
+                                //dataPoints[i] = new DataPoint(i, daily);
+                                values.add(new PointValue(i, daily));
                             }
 
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
 
-                        series = new LineGraphSeries<DataPoint>(dataPoints);
-                        series.setDrawBackground(true);
-                        series.setColor(R.color.graph_blue);
-                        series.setBackgroundColor(R.color.graph_blue);
-                        graph.addSeries(series);
+                        Line line = new Line(values).setColor(R.color.graph_blue);
+                        line.setStrokeWidth(1);
+                        line.setPointRadius(2);
+                        line.setFilled(true);
+                        List<Line> lines = new ArrayList<Line>();
+                        lines.add(line);
 
-                        graph.getViewport().setXAxisBoundsManual(true);
-                        graph.getViewport().setMinX(0);
-                        graph.getViewport().setMaxX(response.length() * 1.005);
+                        LineChartData data = new LineChartData();
+                        data.setLines(lines);
 
-                        graph.getViewport().setYAxisBoundsManual(true);
-                        graph.getViewport().setMinY(0);
-                        graph.getViewport().setMaxY(maxY * 1.1);
+                        Axis axisX = new Axis();
+                        Axis axisY = new Axis().setHasLines(true);
+                        axisX.setTextColor(Color.BLACK);
+                        axisY.setMaxLabelChars(6);
+                        axisY.setTextColor(Color.BLACK);
+                        data.setAxisXBottom(axisX);
+                        data.setAxisYLeft(axisY);
+
+                        //LineChartView chart = new LineChartView(getApplicationContext());
+                        chart.setLineChartData(data);
+
+
+//                        series = new LineGraphSeries<DataPoint>(dataPoints);
+//                        series.setDrawBackground(true);
+//                        series.setColor(R.color.graph_blue);
+//                        series.setBackgroundColor(R.color.graph_blue);
+//                        graph.addSeries(series);
+//
+//                        graph.getViewport().setXAxisBoundsManual(true);
+//                        graph.getViewport().setMinX(0);
+//                        graph.getViewport().setMaxX(response.length() * 1.005);
+//
+//                        graph.getViewport().setYAxisBoundsManual(true);
+//                        graph.getViewport().setMinY(0);
+//                        graph.getViewport().setMaxY(maxY * 1.1);
 
                     }
                 },
@@ -128,5 +172,20 @@ public class CountryStats extends AppCompatActivity {
 
         // Adding JsonObject request to request queue
         AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonArrayRequest, REQUEST_TAG);
+    }
+
+    private class ValueTouchListener implements LineChartOnValueSelectListener {
+
+        @Override
+        public void onValueSelected(int lineIndex, int pointIndex, PointValue value) {
+            Toast.makeText(activity, "Selected: " + value, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onValueDeselected() {
+            // TODO Auto-generated method stub
+
+        }
+
     }
 }
